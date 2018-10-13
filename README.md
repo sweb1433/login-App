d6b20d85dabc194675750d83bb382cec54ff0a5a
sweb1433 <sweb1433@gmail.com>
2018-10-13 19:22:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fcebook-Login-Authentication
+Facebook login authentication using Passport Js.
+
+Run npm install in your project directory. (use node 6)
+1. run sudo mongod
+2. run node server.js
---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID': '333076973920449',
+		'clientSecret': '2405b11d19f330c0e7d94e8dd93b0c3d',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	}
+}
